f8139e067e901cd7e039377703b4b3de7daa6bf3
ARTUROOBREGON13 <arturoobregon13@gmail.com>
2017-09-25 17:23:58 -0500

    Update Maze.hs

--- Maze.hs
+++ Maze.hs
@@ -6,20 +6,19 @@ Modulo para la resolucion de caminos de laberintos
 
 --Localiza el inicio del laberinto marcado con una X y llama a la ruta para generar el conjunto de coordenadas a recorrer
 inicio::[[Char]]->Int->Int->[[Int]]
-inicio arr i j = if((arr!!j!!i) == 'X') 
-then concat [[[i,j]],[(ruta arr i j)]]
-else if i<((length (arr!!j))-1) then inicio arr (i+1) j
-else inicio arr 0 (j+1)
+inicio arr i j = if((arr!!j!!i) == 'X') then concat [[[i,j]], (ruta arr i j)]
+                 else if i<((length (arr!!j))-1) then inicio arr (i+1) j
+                      else inicio arr 0 (j+1)
 
 --Ruta que llama a las funciones mirar Arriba,Abajo, Izquierda y Derecha para determinar donde continua el camino,
 --termina cuando encuentra la posicion que contiene el final marcado con F
---Presenta Error: parse error (possibly incorrect identation or mismatched brackets)
-ruta::[[Char]]->Int->Int->[[Int]] ruta arr i j = if (arr!!j!!i)=='F' then [] else 
-if (lookUp arr i j) then concat [[[i,j-1]],[(ruta arr i (j-1))]] 
-else if (lookDown arr i j) then concat [[[i,j+1]],[(ruta arr i (j+1))]]
-else if (lookLeft arr i j) then concat [[[i-1,j]],[(ruta arr (i-1) j)]]
-else if (lookRight arr i j) then concat [[[i+1,j]],[(ruta arr (i+1) j)]]
-else [[]]
+ruta::[[Char]]->Int->Int->[[Int]] 
+ruta arr i j  |((arr!!j)!!i) == 'F' = [[]] 
+              |lookUp arr i j = concat [[[i,j-1]],(ruta arr i (j-1))] 
+              |lookDown arr i j = concat [[[i,j+1]],(ruta arr i (j+1))]    
+              |lookLeft arr i j = concat [[[i-1,j]],(ruta arr (i-1) j)]   
+              |lookRight arr i j = concat [[[i+1,j]],(ruta arr (i+1) j)]
+                                                                        
 
 --Funciones para ver los valores adyacentes a una posicion especifica.
 lookUp::[[Char]]->Int->Int->Bool
@@ -33,3 +32,4 @@ lookLeft arr i j = if ((arr!!j!!(i-1))=='0' || (arr!!j!!(i-1))=='F') then True e
 
 lookRight::[[Char]]->Int->Int->Bool
 lookRight arr i j = if ((arr!!j!!(i+1))=='0' || (arr!!j!!(i+1))=='F') then True else False
+
